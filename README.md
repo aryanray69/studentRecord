9d77bd36720c2f1747be39a2bf048bcc0ff28cfd
Aryan Ray <aryanoffice69@gmail.com>
2026-07-15 23:50:10 +0530

    latest

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# studentRecord
+# Student Records
+- A simple Student Records web application built using HTML, CSS and JavaScript.
+
+## Functions used 
+1. map()
+2. filter()
+3. addEventListener()
+4. displayStudents()
+5. toLowerCase()
+6. includes()
+7. join()
+8. getElementById()
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## How to Run the Project 
+1. Download or clone the project.
+2. Open the project folder in Visual Studio Code.
+3. Make sure all files are in the same folder.
+4. Open index.html in your browser or you can use the live server extension.
+
+## Author
+**Aryan Ray**
---
+++ script.js
@@ -0,0 +1,89 @@
+const students = [
+    {
+        name: "Aryan",
+        marks: "38%",
+        class: "3rd",
+        address: "India"
+    },
+    {
+        name: "Salmaan Ahmed",
+        marks: "38%",
+        class: "3rd",
+        address: "India"
+    },
+    {
+        name: "Riya Sharma",
+        marks: "85%",
+        class: "10th",
+        address: "123,ABC Colony, Delhi"
+    },
+    {
+        name: "Rohan Patel",
+        marks: "70%",
+        class: "12th",
+        address: "456, XYZ Street Mumbai"
+    },
+    {
+        name: "Priya Singh",
+        marks: "95%",
+        class: "8th",
+        address: "789, PQR Nagar, Bangalore"
+    },
+    {
+        name: "Neha Verma",
+        marks: "80%",
+        class: "11th",
+        address: "222, DEF Avenue, Chennai"
+    },
+    {
+        name: "Manoj Kumar",
+        marks: "75%",
+        class: "10th",
+        address: "333, GHI Lane, Hyderabad"
+    },
+    {
+        name: "Pooja Mishra",
+        marks: "88%",
+        class: "12th",
+        address: "444, STU COLONY Pune"
+    },
+    {
+        name: "Rajesh Singhania",
+        marks: "92%",
+        class: "9th",
+        address: "555, VWX Street, Jaipur"
+    },
+]
+
+const studentContainer = document.getElementById("studentContainer");
+const search = document.getElementById("search");
+
+// Function to display students
+function displayStudents(studentList) {
+
+    studentContainer.innerHTML = studentList.map(student => `
+        <div class="card">
+            <h3>${student.name}</h3>
+            <p><strong>Marks:</strong> ${student.marks}</p>
+            <p><strong>Class:</strong> ${student.class}</p>
+            <p><strong>Address:</strong> ${student.address}</p>
+        </div>
+    `).join("");
+
+}
+
+// Display all students initially
+displayStudents(students);
+
+// Search functionality
+search.addEventListener("input", function () {
+
+    const searchValue = this.value.toLowerCase();
+
+    const filteredStudents = students.filter(student =>
+        student.name.toLowerCase().includes(searchValue)
+    );
+
+    displayStudents(filteredStudents);
+
+});
